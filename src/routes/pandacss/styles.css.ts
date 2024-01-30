4f7f0a2019277447428bcd6bbb71ad418f3f803e
dulee <dulee.dev@gmail.com>
2024-01-30 16:39:54 +0900

    change responsive style

--- src/routes/pandacss/styles.css.ts
+++ src/routes/pandacss/styles.css.ts
@@ -2,16 +2,9 @@ import { css } from '~/styled-system/css';
 
 export const s = {
   h1: css({
-    color: 'secondry',
-    sm: {
-      color: 'primary',
+    color: {
+      base: 'secondry',
+      sm: 'primary',
     },
-
-    // color: {
-    // _dark: {
-    // _disabled: 'secondry',
-    // },
-
-    // },
   }),
 };
